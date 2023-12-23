6de1166f1bd987c003da70cd983b21a7553d271b
faith <faithkunjumon@gmail.com>
2023-12-23 13:50:01 +0000

    first build

---
+++ datasource.tf
@@ -0,0 +1,28 @@
+
+
+data "aws_ami" "latest" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["${var.project_name}-${var.project_env}-*"]
+  }
+
+  filter {
+    name   = "tag:project"
+    values = ["${var.project_name}"]
+  }
+
+  filter {
+    name   = "tag:env"
+    values = ["${var.project_env}"]
+  }
+}
+
+
+data "aws_route53_zone" "mydomain" {
+  name         = var.domain_name
+  private_zone = false
+}
+
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "instance_type" {}
+variable "project_name" {}
+variable "project_env" {}
+variable "domain_name" {}
+variable "hostname" {}
