832283f40560545c994d324e2199b5f23cfae289
faith <faithkunjumon@gmail.com>
2023-12-23 14:11:42 +0000

    first build

--- main.tf
+++ main.tf
@@ -57,7 +57,6 @@ resource "aws_instance" "frontend" {
   ami                    = data.aws_ami.latest.id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.shopping.key_name
-  user_data              = file("setup.sh")
   vpc_security_group_ids = [aws_security_group.frontend.id]
   tags = {
     Name    = "${var.project_name}-${var.project_env}-frontend"
