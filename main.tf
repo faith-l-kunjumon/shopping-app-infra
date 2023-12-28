f5e6f0aff5e3b60a295e042d84b8dc5669ef3d05
faith <faithkunjumon@gmail.com>
2023-12-28 14:49:08 +0000

    version2

--- main.tf
+++ main.tf
@@ -24,6 +24,15 @@ resource "aws_security_group" "frontend" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+ ingress {
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
   ingress {
     from_port        = 443
     to_port          = 443
